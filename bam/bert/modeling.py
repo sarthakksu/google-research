c7b926abb0310e4ab3229b2f9109bd21d3af77fd
Ubuntu <ubuntu@ip-172-31-0-117.us-east-2.compute.internal>
2021-05-13 21:03:08 +0000

    minor change to fix TF version conflict

--- bam/bert/modeling.py
+++ bam/bert/modeling.py
@@ -26,8 +26,8 @@ import math
 import re
 import six
 import tensorflow.compat.v1 as tf
+#from tensorflow.contrib import layers as contrib_layers
 from tensorflow.keras import layers as contrib_layers
-
 
 class BertConfig(object):
   """Configuration for `BertModel`."""
@@ -364,8 +364,9 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return contrib_layers.layer_norm(
-      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  #return contrib_layers.layer_norm(
+  #    inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  return contrib_layers.LayerNormalization(inputs=input_tensor,axis=-1, scope=name)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
